8e9c165642ca46eda8a948ccc1d2947acd41c555
alicantarim <alicantarim@hotmail.com>
2024-10-29 14:44:32 +0300

    adds Menu struct and arrays

---
+++ MenuApp/MenuItem.swift
@@ -0,0 +1,15 @@
+//
+//  MenuItem.swift
+//  MenuApp
+//
+//  Created by Alican TARIM on 29.10.2024.
+//
+
+import Foundation
+
+struct MenuItem {
+    
+    var name: String
+    var price: String  // The reason for this is because I want to include the currency symbol on it.
+    var imageName: String
+}
