809278610c25a02a0f9bdab696dbb10ac073a112
alicantarim <alicantarim@hotmail.com>
2024-10-29 13:49:53 +0300

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Alican TARIM on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
